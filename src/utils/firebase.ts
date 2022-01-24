3c1c020bf771c7cb8de5f290e78913dd5a4d90c0
Mary McGrath <m.c.mcgrath13@gmail.com>
2022-01-24 16:32:02 -0500

    build: clean up deps, nominally add firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDPQ4bLraU1JOyHyJnOZR6vwpOMzFG-14c",
+//   authDomain: "signal-ri.firebaseapp.com",
+//   projectId: "signal-ri",
+//   storageBucket: "signal-ri.appspot.com",
+//   messagingSenderId: "677604573225",
+//   appId: "1:677604573225:web:3292361b3481fbdd7bdc38",
+//   measurementId: "G-Y4BWT9PHG2",
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
