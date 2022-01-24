384dd6fc785dd05379ab3d240569942dd37d59a6
Mary McGrath <m.c.mcgrath13@gmail.com>
2022-01-24 16:44:36 -0500

    build: remove unused style sheets

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import App from "./App.vue";
 import router from "./router";
 
 // general styling
-import "../node_modules/vue-multiselect/dist/vue-multiselect.css";
-import "../node_modules/mapbox-gl/dist/mapbox-gl.css";
 import "@/assets/styles/main.scss";
 
 createApp(App).use(router).use(createPinia()).mount("#app");
