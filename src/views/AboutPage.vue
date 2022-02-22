f7b373100ea01fff554725af53ea616cb835d868
Sam Bessey <sam_bessey@brown.edu>
2022-02-22 13:35:22 -0500

    draft about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -5,26 +5,14 @@
       <template #subtitle>
         <div class="content">
           <p>
-            The National Science Foundation in 2017 awarded the University of
-            Rhode Island with a grant to establish a statewide research
-            consortium — the RI Consortium for Coastal Ecology Assessment,
-            Innovation, and Modeling (RI C-AIM) — to study the effects of
-            climate variability on coastal ecosystems. The RI Data Discovery
-            Center is one of the efforts of RI C-AIM consortium. The goal of the
-            RI Data Discovery Center is to become the national and international
-            go-to-source for data on the Narragansett Bay ecosystem. For C-AIM
-            investigators pursuing the research goals of the Integrated Bay
-            Observatory, Predicting Ecosystem Response and Visualization &
-            Imaging, RI Data Discovery Center will become the site where they
-            will store their data, share their data internally with other C-AIM
-            investigators and share their data externally with investigators
-            around the world. In addition to new data collected by C-AIM
-            investigators, RI Data Discovery Center will also collect and share
-            historical data on the Narragansett Bay ecosystem. In addition to
-            sharing data with scientists, RIDDC will also become the
-            go-to-source where decision makers, land-use managers, relevant
-            industries, citizen scientists and students can find data on the
-            Narragansett Bay ecosystem.
+            All Rhode Islanders deserve the same opportunity to live a healthy
+            and happy life. However, all communities do not have the same
+            resources at home, school, and work to do this. These differences
+            mean that some communities have been more likely to suffer from
+            COVID-19 than others. At Project Signal, we use data to show how
+            COVID-19 has impacted the hardest-hit neighborhoods. We make these
+            data available so we can make policies that protect the most
+            vulnerable Rhode Islanders.
           </p>
         </div>
       </template>
