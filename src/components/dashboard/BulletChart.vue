11dc0d8af8618d4602dad3f505fe3c4ebdac4ba8
Mary McGrath <m.c.mcgrath13@gmail.com>
2022-02-24 14:25:47 -0500

    feat: maybe this is a fancy bar chart now?

--- src/components/dashboard/BulletChart.vue
+++ src/components/dashboard/BulletChart.vue
@@ -5,6 +5,7 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import { useVega } from "../../composables/useVega";
+import { COLORS } from "../../utils/constants";
 
 interface Stat {
   name: string;
@@ -30,9 +31,14 @@ const fieldData = computed(() => {
   return props.fieldNames.map((f) => ({
     name: upperFirst(f),
     actualField: `doses_${f}`,
+    expectedField: `doses_to_close_gap_${f}`,
     populationField: `population_${f}`,
   }));
 });
+
+// TODO: this is temporary as this should be fixed on the data side
+const parseNum = (val) =>
+  typeof val === "string" ? parseInt(val.replaceAll(",", "")) : val;
 
 const activeStats = computed(() => {
   const row = props.stats.find((stat) => stat.name === props.activeCluster);
@@ -41,7 +47,11 @@ const activeStats = computed(() => {
     // don't let a population be more than 100% vaccinated
     return fieldData.value.map((f) => ({
       name: f.name,
-      pct: Math.min(1, row[f.actualField] / row[f.populationField]),
+      pct: Math.min(
+        1,
+        parseNum(row[f.actualField]) / parseNum(row[f.populationField])
+      ),
+      gap: parseNum(row[f.expectedField]),
     }));
   } else {
     return [];
@@ -52,6 +62,7 @@ const spec = computed(() => {
   return {
     $schema: "https://vega.github.io/schema/vega/v5.json",
     background: "transparent",
+    padding: { left: 5, top: 0, right: 5, bottom: 0 },
 
     data: [
       {
@@ -75,30 +86,41 @@ const spec = computed(() => {
         type: "band",
         domain: { data: "yFields", field: "name" },
         range: "height",
-        padding: 0.05,
+        padding: 0.2,
+      },
+      {
+        name: "color",
+        type: "ordinal",
+        domain: { data: "yFields", field: "name" },
+        range: [COLORS.pink, COLORS.beige, COLORS.green, COLORS.info],
       },
     ],
 
     axes: [
-      { orient: "bottom", scale: "xscale" },
-      { orient: "left", scale: "yscale" },
+      {
+        orient: "bottom",
+        scale: "xscale",
+        format: ".0%",
+        labelOverlap: "parity",
+        labelSeparation: 4,
+        domain: false,
+        ticks: false,
+      },
+      {
+        orient: "left",
+        scale: "yscale",
+        ticks: false,
+        domainWidth: 1,
+        domainColor: COLORS.dark,
+        labelFontWeight: 700,
+        labelPadding: 5,
+        offset: 1,
+      },
     ],
 
     marks: [
       {
-        type: "rect",
-        from: { data: "yFields" },
-        encode: {
-          enter: {
-            x: { scale: "xscale", value: expected.value },
-            x2: { scale: "xscale", value: 0 },
-            y: { scale: "yscale", field: "name" },
-            height: { scale: "yscale", band: 1 },
-            opacity: { value: 0.5 },
-          },
-        },
-      },
-      {
+        name: "bars",
         type: "rect",
         from: { data: "stats" },
         encode: {
@@ -108,7 +130,48 @@ const spec = computed(() => {
             yc: {
               signal: "scale('yscale', datum.name) + bandwidth('yscale') / 2",
             },
-            height: { scale: "yscale", band: 0.4 },
+            height: { scale: "yscale", band: 1 },
+            fill: { scale: "color", field: "name" },
+          },
+          update: {
+            opacity: { value: 0.9 },
+            tooltip: {
+              signal:
+                "{ title: datum.name, 'Percent Vaccinated': format(datum.pct, '.0%'), 'Doses to Close Gap': datum.gap}",
+            },
+          },
+          hover: {
+            opacity: { value: 1.0 },
+          },
+        },
+      },
+      {
+        type: "text",
+        from: { data: "bars" },
+        encode: {
+          enter: {
+            x: { field: "x2", offset: 5 },
+            y: { field: "y", offset: { field: "height", mult: 0.5 } },
+            fill: { value: COLORS.dark },
+            align: { value: "left" },
+            baseline: { value: "middle" },
+            text: {
+              signal:
+                "datum.datum.gap > 0 ? datum.datum.gap + ' dose gap' : ''",
+            },
+          },
+        },
+      },
+      {
+        type: "rule",
+        encode: {
+          enter: {
+            x: { scale: "xscale", value: expected.value },
+            y: { value: 0 },
+            y2: { signal: "height" },
+            stroke: { value: COLORS.dark },
+            strokeDash: { value: [2, 2] },
+            strokeWidth: { value: 1 },
           },
         },
       },
@@ -120,7 +183,7 @@ const el = ref(null);
 useVega({
   spec,
   el,
-  minHeight: ref(200),
+  minHeight: ref(150),
   maxHeight: ref(1280),
   maxWidth: ref(1280),
   includeActions: ref(true),
