978ff417cf39e0e641ed65d202ffee549f429ad2
Sam Bessey <sam_bessey@brown.edu>
2022-03-02 15:34:03 -0500

    add About Us and Disclosures

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -5,61 +5,75 @@
       <template #subtitle>
         <div class="content">
           <p>
-            All Rhode Islanders deserve the same opportunity to live a healthy
-            and happy life. However, all communities do not have the same
-            resources at home, school, and work to do this. These differences
-            mean that some communities have been more likely to suffer from
-            COVID-19 than others. At Project Signal, we use data to show how
-            COVID-19 has impacted the hardest-hit neighborhoods. We make these
-            data available so we can make policies that protect the most
-            vulnerable Rhode Islanders.
+            All Rhode Islanders have the right to live a healthy and happy life.
+            We all want resources to help us and our families thrive at home,
+            work, or school. But we don’t all get these same opportunities
+            because of where we live. This means that some communities are
+            affected by COVID-19 more than others. At Project Signal, we use
+            data to show how COVID-19 has impacted the hardest-hit
+            neighborhoods. We make this data available to support policies that
+            protect the most vulnerable Rhode Islanders.
           </p>
         </div>
       </template>
     </DashboardCard>
     <DashboardCard>
-      <template #title>Credits</template>
+      <template #title>About Us</template>
       <template #subtitle>
         <div class="content">
           <p>
-            The historical data available for lookup on this site has been
-            compiled from quality controlled data from Narragansett Bay Fixed
-            Site Monitoring Network (NBFSMN). The RI DEM Office of Water
-            Resources (RIDEM-OWR) has been leading the effort of coordinating
-            this multi-agency collaboration. Folllowing agencies contributed to
-            the network of fixed-site monitoring stations:
+            Project SIGNAL is a collaboration between the Rhode Island
+            Department of Health and the People, Place, and Health Collective in
+            the Department of Epidemiology at the
+            <a href="https://www.brown.edu/academics/public-health/home"
+              >Brown University School of Public Health.</a
+            >
+            The People, Place, and Health Collective conducts research to
+            support public health policies and programs that are data-driven,
+            people-centered, and place-oriented. Some of the People, Place, and
+            Health Collective's other signature initiatives include
+            <a href="https://www.preventoverdoseri.org/">PreventOverdoseRI</a>,
+            <a href="https://profoundmodel.web.app/">PROFOUND</a>,
+            <a href="http://provident.preventoverdoseri.org/">PROVIDENT</a>, and
+            <a href="https://www.titanmodel.org/">TITAN</a>. For more about the
+            People, Place, and Health Collective, visit our website
+            <a href="https://medium.com/pphc">here</a>.
           </p>
-          <ul class="list">
-            <li>
-              Rhode Island Department of Environmental Management- Office of
-              Water Resources (RIDEM-OWR)
-            </li>
-            <li>
-              University of Rhode Island, Graduate School of Oceanography
-              (URI-GSO)
-            </li>
-            <li>Narragansett Bay Commission (NBC)</li>
-            <li>
-              Narragansett Bay National Estuarine Research Reserve (NBNERR)
-            </li>
-            <li>Roger Williams University (RWU)</li>
-            <li>
-              Narragansett Bay Estuary Program (NBNEP), and URI Coastal
-              Institute
-            </li>
-          </ul>
           <p>
-            If you use any of these historical data in any publication,
-            presentation, poster, media production or similar, please remember
-            that you need to cite the data sources listed on RI DEM Fixed-Site
-            Monitoring Stations Network Data (look under Citation for Data
-            users)
+            <a style="padding-left: 20px" href="#"
+              ><a href="https://github.com/pph-collective"
+                ><i class="fab fa-github"></i></a
+            ></a>
+            <a style="padding-left: 25px" href="#"
+              ><a href="https://twitter.com/pph_collective"
+                ><i class="fab fa-twitter"></i></a
+            ></a>
+            <a style="padding-left: 25px" href="#"
+              ><a href="https://www.instagram.com/pph_collective"
+                ><i class="fab fa-instagram"></i></a
+            ></a>
           </p>
-
+        </div>
+      </template>
+    </DashboardCard>
+    <DashboardCard>
+      <template #title>Disclosures</template>
+      <template #subtitle>
+        <div class="content">
           <p>
-            For more information about the RI DEM Fixed-Site Monitoring Stations
-            please refer to RI DEM Fixed-Site Monitoring Stations and Data in
-            Narragansett Bay
+            Project SIGNAL leverages the Rhode Island Department of Health's
+            multi-component surveillance system for monitoring patterns in
+            COVID-19 and related outcomes. Datasets used to create the contents
+            of this web tool were obtained through an approved request to the
+            Rhode Island Department of Health. This web tool was developed using
+            funds from the Centers for Disease Control and Prevention's National
+            Initiative to Address COVID-19 Health Disparities Among Populations
+            at High-Risk and Underserved (OT21-2103) via the Rhode Island
+            Department of Health. Any opinions, findings, conclusions or
+            recommendations expressed in this material are those of the
+            author(s) and do not necessarily reflect the views of the Centers
+            for Disease Control and Prevention or the Rhode Island Department of
+            Health.
           </p>
         </div>
       </template>
