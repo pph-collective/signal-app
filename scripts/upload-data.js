bf57052b59a7cca2348487fd59225aace8ea0913
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2022-04-01 10:30:31 -0400

    refactor: abstract upload script, add bariers file

--- scripts/upload-data.js
+++ scripts/upload-data.js
@@ -9,6 +9,7 @@
  * node ./scripts/upload-data.js --id vax_first_dose_coldspots --date 2022-03-15 --geojson ./data/vaccine_coldspot_polygons_03_15_2022.geojson --statsfile ./data/vaccine_coldspot_statistics_03_15_2022.json
  */
 
+/* eslint "@typescript-eslint/no-var-requires": "off" */
 const fs = require("fs");
 const path = require("path");
 const { ArgumentParser } = require("argparse");
@@ -25,8 +26,8 @@ const warnAndExit = (warning) => {
 
 const getJson = (jsonFile) => {
   const rawdata = fs.readFileSync(jsonFile);
-  return JSON.parse(rawdata)
-}
+  return JSON.parse(rawdata);
+};
 
 const argparse = new ArgumentParser({
   description: "SIGNAL - upload data to Firestore",
@@ -35,22 +36,27 @@ const argparse = new ArgumentParser({
 
 argparse.add_argument("-i", "--id", {
   required: true,
-  help: "ID of dataset for the collection"
+  help: "ID of dataset for the collection",
 });
 
 argparse.add_argument("-d", "--date", {
   required: true,
-  help: "Date of dataset, formatted in yyyy-mm-dd"
+  help: "Date of dataset, formatted in yyyy-mm-dd",
 });
 
 argparse.add_argument("-g", "--geojson", {
   required: true,
-  help: "Path to geojson shape file"
-})
+  help: "Path to geojson shape file",
+});
 
 argparse.add_argument("-s", "--statsfile", {
   required: true,
   help: "Path to stats json file",
+});
+
+argparse.add_argument("-b", "--barriersfile", {
+  required: true,
+  help: "Path to barriers json file",
 });
 
 argparse.add_argument("-o", "--overwrite", {
@@ -60,35 +66,57 @@ argparse.add_argument("-o", "--overwrite", {
 
 argparse.add_argument("-n", "--newId", {
   action: "store_true",
-  help: "if the collection id does not exist, creates a new collection"
+  help: "if the collection id does not exist, creates a new collection",
 });
 
 argparse.add_argument("-l", "--localDir", {
-  help: "path to local folder to save downloaded files to"
-})
+  help: "path to local folder to save downloaded files to",
+});
 
 const main = async () => {
-  const { newId, overwrite, geojson, statsfile, id, date, localDir } = argparse.parse_args();
+  const {
+    newId,
+    overwrite,
+    geojson,
+    statsfile,
+    barriersfile,
+    id,
+    date,
+    localDir,
+  } = argparse.parse_args();
 
   if (!dateRegex.test(date)) {
     warnAndExit(`ERROR! Incorrect date format. Use yyyy-mm-dd format: ${date}`);
   }
 
-  if (!fs.existsSync(geojson)) {
-    warnAndExit(`ERROR! File does not exist: ${geojson}`);
-  }
+  const files = [
+    {
+      filePath: geojson,
+      extension: "geojson",
+      collection: "geo",
+      property: "features",
+    },
+    {
+      filePath: statsfile,
+      extension: "json",
+      collection: "stats",
+    },
+    {
+      filePath: barriersfile,
+      extension: "json",
+      collection: "barriers",
+    },
+  ];
 
-  if (path.extname(geojson).toUpperCase() !== ".GEOJSON"){
-    warnAndExit(`ERROR! Expected a .geojson file: ${geojson}`);
-  }
+  files.forEach(({ filePath, extension }) => {
+    if (!fs.existsSync(filePath)) {
+      warnAndExit(`ERROR! File does not exist: ${filePath}`);
+    }
 
-  if (!fs.existsSync(statsfile)) {
-    warnAndExit(`ERROR! File does not exist: ${statsfile}`);
-  }
-
-  if (path.extname(statsfile).toUpperCase() !== ".JSON") {
-    warnAndExit(`ERROR! Expected the stats file to be a json file: ${statsfile}`);
-  }
+    if (path.extname(filePath).toLowerCase() !== `.${extension}`) {
+      warnAndExit(`ERROR! Expected a .${extension} file: ${filePath}`);
+    }
+  });
 
   process.env.GOOGLE_APPLICATION_CREDENTIALS = "serviceAccount.json";
   const app = initializeApp();
@@ -98,9 +126,13 @@ const main = async () => {
   const collections = (await db.listCollections()).map((c) => c.id);
   if (!collections.includes(id)) {
     if (newId) {
-      console.warn(`WARNING! id does not exist in firestore. This script will create the following collection: ${id}`);
+      console.warn(
+        `WARNING! id does not exist in firestore. This script will create the following collection: ${id}`
+      );
     } else {
-      warnAndExit(`ERROR! id must match an existing collection id: ${collections.join(", ")}.
+      warnAndExit(`ERROR! id must match an existing collection id: ${collections.join(
+        ", "
+      )}.
       Use the --newId flag to create a new collection id for "${id}"`);
     }
   }
@@ -108,15 +140,19 @@ const main = async () => {
   const docPath = `${id}/${date}`;
   const docRef = db.collection(id).doc(date);
 
-  const dir = ` ${localDir}/${docPath}`;
+  const dir = `${localDir}/${docPath}`;
 
   if (localDir) {
     // Check if directory exists locally
     if (fs.existsSync(dir)) {
       if (overwrite) {
-        console.warn(`WARNING! Directory exists locally. Overwriting... ${dir}`);
+        console.warn(
+          `WARNING! Directory exists locally. Overwriting... ${dir}`
+        );
       } else {
-        warnAndExit(`ERROR!: Directory exists locally. Use the overwrite flag if you wish to continue: ${dir}`);
+        warnAndExit(
+          `ERROR!: Directory exists locally. Use the overwrite flag if you wish to continue: ${dir}`
+        );
       }
     }
   } else {
@@ -124,15 +160,26 @@ const main = async () => {
     const docSnapshot = await docRef.get();
     if (docSnapshot.exists) {
       if (overwrite) {
-        console.warn(`WARNING! Document exists in Firestore. Overwriting... ${docPath}`);
+        console.warn(
+          `WARNING! Document exists in Firestore. Overwriting... ${docPath}`
+        );
       } else {
-        warnAndExit(`ERROR!: Document exists in Firestore. Use the overwrite flag if you wish to continue: ${docPath}`);
+        warnAndExit(
+          `ERROR!: Document exists in Firestore. Use the overwrite flag if you wish to continue: ${docPath}`
+        );
       }
     }
   }
 
-  const geo = getJson(geojson).features;
-  const stats = getJson(statsfile);
+  // read in the files to the data property on each file object
+  files.forEach((file) => {
+    const json = getJson(file.filePath);
+    if (file.property) {
+      file.data = json[file.property];
+    } else {
+      file.data = json;
+    }
+  });
 
   if (localDir) {
     // Make directory, no harm done if already exists
@@ -141,28 +188,28 @@ const main = async () => {
         warnAndExit(err);
       }
 
-      const geoPath = `${dir}/geo.json`;
-      const statsPath = `${dir}/stats.json`;
+      files.forEach(({ collection, data }) => {
+        const path = `${dir}/${collection}.json`;
 
-      fs.writeFile(geoPath, JSON.stringify(geo), (err) => {
-        if (err) {
-          warnAndExit(err);
-        }
-        console.log(`SUCCESS! Created file: ${geoPath}`);
-      });
-
-      fs.writeFile(statsPath, JSON.stringify(stats), (err) => {
-        if (err) {
-          warnAndExit(err);
-        }
-        console.log(`SUCCESS! Created file: ${statsPath}`);
+        fs.writeFile(path, JSON.stringify(data), (err) => {
+          if (err) {
+            warnAndExit(err);
+          }
+          console.log(`SUCCESS! Created file: ${path}`);
+        });
       });
     });
   } else {
+    console.log(keys);
     await docRef.set({
-      geo: stringify(geo),
-      stats: stringify(stats),
-      last_updated: Date.now()
+      ...files.reduce(
+        (previous, { data, collection }) => ({
+          [collection]: stringify(data),
+          ...previous,
+        }),
+        {}
+      ),
+      last_updated: Date.now(),
     });
 
     console.log(`SUCCESS! Created document in firestore: ${docPath}`);
