df6a5f59da74dd941d48df9d321c14bf418e3561
Ellen Duong <shi.ellenduong@gmail.com>
2022-05-26 13:32:46 -0400

    build: npm update

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+    'vue/multi-word-component-names': 'off'
   },
   parser: "vue-eslint-parser",
   parserOptions: {
