a5ee9f9b4068b305e088268feee2a54baede07bd
Sam Bessey <sam_bessey@brown.edu>
2022-05-27 11:39:53 -0400

    refactor: make VaccineResources component

--- src/views/datasets/vaccine-gap/dashboard.vue
+++ src/views/datasets/vaccine-gap/dashboard.vue
@@ -116,95 +116,7 @@
   <DashboardCard width="full">
     <template #title>What can I do to close the gap?</template>
     <template #content>
-      <div class="px-4 content">
-        <h5>
-          I can raise awareness and address my community's concerns about
-          vaccines:
-        </h5>
-        <ul>
-          <li>
-            You can
-            <ExternalLink
-              href="https://health.ri.gov/publications/actionplans/COVID-vaccine-outreach.pdf"
-              >educate your community about vaccines and build trust </ExternalLink
-            >. This document has videos and messages you can share with people
-            in your community.
-          </li>
-          <li>
-            You can also promote
-            <ExternalLink
-              href="https://drive.google.com/drive/folders/11NgvrZgqLs34MO2qicAp48Cnn5O_5Cg7"
-              >resources for communities with special needs </ExternalLink
-            >.
-          </li>
-        </ul>
-        <h5>I can promote upcoming vaccine clinics in my neighborhood:</h5>
-        <ul>
-          <li>
-            First, check the
-            <ExternalLink
-              href="https://docs.google.com/spreadsheets/d/e/2PACX-1vSv20xMlWQJON8jXvPtzdAo1KU-Rr6Eu6u02nUuVEu-Bz2t_M6JBzYLfFaL_jPZqLiAQZVigFcssKwx/pubhtml"
-              >list of upcoming vaccine clinics
-            </ExternalLink>
-            to get information about the clinic(s) you want to promote.
-          </li>
-          <li>
-            Next, you can create
-            <ExternalLink
-              href="https://drive.google.com/drive/folders/1ZpYrchu-srZJJSmn-yMDRPw-LfgbWIzB"
-              >promotional materials and promote
-            </ExternalLink>
-            upcoming clinics. This will help you let people in your community
-            know what is happening.
-          </li>
-          <li>
-            You can also let people know
-            <ExternalLink href="https://covid.ri.gov/vaccination#athome"
-              >how to get vaccinated at home </ExternalLink
-            >.This is a good option for people who may have a harder time
-            accessing a clinic.
-          </li>
-        </ul>
-        <h5>I can set up a new vaccine clinic in my neighborhood:</h5>
-        <ul>
-          <li>
-            Get started by
-            <ExternalLink
-              href="https://forms.office.com/Pages/ResponsePage.aspx?id=VGrKUmVENUa_82XQqEEiiDhbUGCR_dNCilNlrWlEGs1UN1A3UVY1QlhJQVZJNTFFTTM4WUkwREtEMS4u"
-              >filling out an interest form </ExternalLink
-            >. A member of the RIDOH team will reach out to you within 3-5
-            business days.
-          </li>
-          <li>
-            Next, you can create
-            <ExternalLink
-              href="https://drive.google.com/drive/folders/1ZpYrchu-srZJJSmn-yMDRPw-LfgbWIzB"
-              >promotional materials and promote
-            </ExternalLink>
-            your clinic. This toolkit has resources that you may need to reach
-            people in your neighborhood.
-          </li>
-        </ul>
-        <h5>
-          I can make sure people know where to go to get treatment for COVID-19:
-        </h5>
-        <ul>
-          <li>
-            Find out
-            <ExternalLink
-              href="https://covid-19-test-to-treat-locator-dhhs.hub.arcgis.com/"
-              >where you can find treatment in your neighborhood </ExternalLink
-            >.
-          </li>
-        </ul>
-        <h5>
-          I can find more resources at the
-          <ExternalLink
-            href="https://covid.ri.gov/public/covid-19-community-partner-toolkit"
-            >Community Partner Toolkit website </ExternalLink
-          >.
-        </h5>
-      </div>
+      <VaccineResources></VaccineResources>
     </template>
   </DashboardCard>
 
@@ -297,6 +209,7 @@ import { useQueryParam } from "../../../composables/useQueryParam";
 import { fetchColdSpotData } from "../../../utils/firebase";
 import { NULL_CLUSTER } from "../../../utils/constants";
 import { prettyDate } from "../../../utils/utils";
+import VaccineResources from "../../../components/dashboard/VaccineResources.vue";
 
 const zoomed = ref(false);
 useQueryParam({
