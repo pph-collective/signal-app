ca379b836e6c0c8e6fed25b5a260df70d0c5d100
Sam Bessey <sam_bessey@brown.edu>
2022-08-03 11:38:54 -0400

    feat: add housing grouped bar chart

---
+++ src/components/dashboard/GroupBarChart.vue
@@ -0,0 +1,44 @@
+<template>
+  <div ref="el"></div>
+</template>
+
+<script setup lang="ts">
+import { useVega } from "../../composables/useVega";
+import { computed, ref } from "vue";
+
+const spec = computed(() => {
+  return {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    data: {
+      values: [
+        { category: "A", group: "x", value: 0.1 },
+        { category: "A", group: "y", value: 0.6 },
+        { category: "A", group: "z", value: 0.9 },
+        { category: "B", group: "x", value: 0.7 },
+        { category: "B", group: "y", value: 0.2 },
+        { category: "B", group: "z", value: 1.1 },
+        { category: "C", group: "x", value: 0.6 },
+        { category: "C", group: "y", value: 0.1 },
+        { category: "C", group: "z", value: 0.2 },
+      ],
+    },
+    mark: "bar",
+    encoding: {
+      x: { field: "category" },
+      y: { field: "value", type: "quantitative" },
+      xOffset: { field: "group" },
+      color: { field: "group" },
+    },
+  };
+});
+
+const el = ref(null);
+useVega({
+  spec,
+  el,
+  minHeight: ref(180),
+  maxHeight: ref(1280),
+  maxWidth: ref(1280),
+  includeActions: ref(false),
+});
+</script>
--- src/views/spotlights/housing/dashboard.vue
+++ src/views/spotlights/housing/dashboard.vue
@@ -3,7 +3,12 @@
     <template #subtitle> This has something to do with housing. </template>
   </DashboardCard>
   <DashboardCard width="two-thirds">
-    <template #content>This might be a graph</template>
+    <template #content>
+      <!-- not sure if i need this div? doesn't seem to change anything -->
+      <div class="signal-grid-container">
+        <GroupBarChart />
+      </div>
+    </template>
   </DashboardCard>
   <DashboardCard width="one-third">
     <template #content>explain the graph?</template>
@@ -12,4 +17,5 @@
 
 <script setup lang="ts">
 import DashboardCard from "@/components/base/DashboardCard.vue";
+import GroupBarChart from "@/components/dashboard/GroupBarChart.vue";
 </script>
