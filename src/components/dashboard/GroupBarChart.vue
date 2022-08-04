f92cc89f97045bddf1192d06416ae008a29d0f2a
Sam Bessey <sam_bessey@brown.edu>
2022-08-04 09:35:56 -0400

    refactor: change bar chart type

--- src/components/dashboard/GroupBarChart.vue
+++ src/components/dashboard/GroupBarChart.vue
@@ -8,29 +8,103 @@ import { computed, ref } from "vue";
 
 const spec = computed(() => {
   return {
-    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    $schema: "https://vega.github.io/schema/vega/v5.json",
+    width: 300,
+    height: 240,
+    padding: 5,
     data: {
+      name: "table",
       values: [
-        { category: "A", group: "x", value: 0.1 },
-        { category: "A", group: "y", value: 0.6 },
-        { category: "A", group: "z", value: 0.9 },
-        { category: "B", group: "x", value: 0.7 },
-        { category: "B", group: "y", value: 0.2 },
-        { category: "B", group: "z", value: 1.1 },
-        { category: "C", group: "x", value: 0.6 },
-        { category: "C", group: "y", value: 0.1 },
-        { category: "C", group: "z", value: 0.2 },
+        { category: "A", position: 0, value: 0.1 },
+        { category: "A", position: 1, value: 0.6 },
+        { category: "A", position: 2, value: 0.9 },
+        { category: "A", position: 3, value: 0.4 },
+        { category: "B", position: 0, value: 0.7 },
+        { category: "B", position: 1, value: 0.2 },
+        { category: "B", position: 2, value: 1.1 },
+        { category: "B", position: 3, value: 0.8 },
+        { category: "C", position: 0, value: 0.6 },
+        { category: "C", position: 1, value: 0.1 },
+        { category: "C", position: 2, value: 0.2 },
+        { category: "C", position: 3, value: 0.7 },
       ],
     },
-    mark: "bar",
-    encoding: {
-      x: { field: "category" },
-      y: { field: "value", type: "quantitative" },
-      xOffset: { field: "group" },
-      color: { field: "group" },
-    },
-    width: 400,
-    height: 400,
+    scales: [
+      {
+        name: "yscale",
+        type: "band",
+        domain: { data: "table", field: "category" },
+        range: "height",
+        padding: 0.2,
+      },
+      {
+        name: "xscale",
+        type: "linear",
+        domain: { data: "table", field: "value" },
+        range: "width",
+        round: true,
+        zero: true,
+        nice: true,
+      },
+      {
+        name: "color",
+        type: "ordinal",
+        domain: { data: "table", field: "position" },
+        range: { scheme: "category20" },
+      },
+    ],
+    axes: [
+      {
+        orient: "left",
+        scale: "yscale",
+        tickSize: 0,
+        labelPadding: 4,
+        zindex: 1,
+      },
+      { orient: "bottom", scale: "xscale" },
+    ],
+    marks: [
+      {
+        type: "group",
+        from: {
+          facet: {
+            data: "table",
+            name: "facet",
+            groupby: "category",
+          },
+        },
+        encode: {
+          enter: {
+            y: { scale: "yscale", field: "category" },
+          },
+        },
+        signals: [{ name: "height", update: "bandwidth('yscale')" }],
+        scales: [
+          {
+            name: "pos",
+            type: "band",
+            range: "height",
+            domain: { data: "facet", field: "position" },
+          },
+        ],
+        marks: [
+          {
+            name: "bars",
+            from: { data: "facet" },
+            type: "rect",
+            encode: {
+              enter: {
+                y: { scale: "pos", field: "position" },
+                height: { scale: "pos", band: 1 },
+                x: { scale: "xscale", field: "value" },
+                x2: { scale: "xscale", value: 0 },
+                fill: { scale: "color", field: "position" },
+              },
+            },
+          },
+        ],
+      },
+    ],
   };
 });
 
