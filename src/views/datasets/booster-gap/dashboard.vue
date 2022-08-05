dbda37f1342efeba93e471477dc3c9461abed302
Sam Bessey <sam_bessey@brown.edu>
2022-08-05 11:29:50 -0400

    refactor: change zoom button color

--- src/views/datasets/booster-gap/dashboard.vue
+++ src/views/datasets/booster-gap/dashboard.vue
@@ -30,7 +30,7 @@
     <template #top-right>
       <button
         :disabled="!activeCluster.name"
-        class="zoom-button button is-secondary is-light"
+        class="zoom-button button is-link"
         @click="zoomed = !zoomed"
       >
         <span class="icon">
@@ -179,7 +179,7 @@ import DataNotes from "@/components/dashboard/DataNotes.vue";
 import { useQueryParam } from "../../../composables/useQueryParam";
 
 import { fetchColdSpotData } from "../../../utils/firebase";
-import { NULL_CLUSTER } from "../../../utils/constants";
+import { NULL_CLUSTER, COLORS } from "../../../utils/constants";
 import { prettyDate } from "../../../utils/utils";
 import VaccineResources from "../../../components/dashboard/VaccineResources.vue";
 
