96fee22f25ec9bbcd4adf577a0220ab10466f6ee
Sam Bessey <sam_bessey@brown.edu>
2023-04-17 12:56:30 -0400

    change bivalent text based on date

--- src/views/datasets/booster-gap/dashboard.vue
+++ src/views/datasets/booster-gap/dashboard.vue
@@ -1,7 +1,8 @@
 <template>
   <DashboardCard width="full">
     <template #subtitle>
-      This tool shows us places where fewer people have received a new bivalent
+      This tool shows us places where fewer people have received a
+      {{ currentDate >= "2023-03-15" ? "new bivalent" : "" }}
       booster dose compared to state levels. We call this difference in booster
       doses a
       <em>gap</em>. Vaccines can keep us from getting very sick with COVID-19,
