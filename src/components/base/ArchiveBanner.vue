67d4f02df1234e8ef41be5c38fef091e58de7f71
Ellen Duong <shi.ellenduong@gmail.com>
2026-05-06 09:45:56 -0400

    refactor: archive banner wording

--- src/components/base/ArchiveBanner.vue
+++ src/components/base/ArchiveBanner.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="banner has-text-centered">
-    This content is part of our web archive. It is no longer maintained and may
-    not meet current accessibility standards.
+    This documentation contains archived materials maintained for reference or
+    record-keeping purposes. Some materials may not meet current accessibility
+    standards. To learn more or to request an accessible version of archived
+    content, please visit:
+    <a href="https://digital-accessibility.brown.edu/" target="_blank">
+      Digital Accessibility at Brown University
+    </a>
   </div>
 </template>
 
@@ -13,5 +18,20 @@
     vertical-align: middle;
     padding-block: 5px;
     font-size: 1.2rem;
+}
+
+.banner a {
+  color: #ffff66;
+  text-decoration: underline;
+  font-weight: bold;
+  transition: color 0.2s;
+}
+
+.banner a:hover,
+.banner a:focus {
+  color: #ffffff;
+  /* background-color: #b22222; */
+  outline: 2px solid #ffffff;
+  border-radius: 2px;
 }
 </style>
